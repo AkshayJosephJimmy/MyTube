e560ec17eb8cb74d7386a5cea7c29eb730f2cee2
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2025-08-26 12:11:33 +0530

    added the tags in Modal .skeleton Modal form comepelete for create Project

--- Mytube_frontend/src/components/Modal.jsx
+++ Mytube_frontend/src/components/Modal.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Tags from './Tags';
 
 function Modal({isOpen,setIsOpen}) {
+    
+    const [tag,setTag]=React.useState("");
+    const [tags,setTags]=React.useState([]);
     
 function handleClose(){
     setIsOpen(false)
 }
+
+function handleTag(e){
+setTag(e.target.value)
+
+}
+function handleTagAdd(e){
+e.preventDefault();
+if(tag==="" || tags.includes(tag)) return;
+
+    setTags([...tags,tag])
+}
+function removeTag(t){
+    setTags(tags.filter(tag=>tag!==t))
+}
+    
   
 
 
@@ -15,9 +34,22 @@ if(isOpen){
     return ReactDOM.createPortal(
         <div className='fixed top-0 left-0 w-full h-full bg-black opacity-50 flex items-center justify-center' onClick={()=>handleClose()}>
 
-    <div className='bg-white p-4 rounded'>
-        <form>
-            
+    <div className='bg-white p-4 rounded' onClick={(e)=>e.stopPropagation()}>
+        <form className='flex flex-col gap-4'>
+            <label for='name'>Project Name</label>
+            <input type='text' id='name' className='border-2 border-black'/>
+            <label for='description'>Description</label>
+            <input type='text' id='description' className='border-2 border-black'/>
+            <label for='tags'>Tags</label>
+            <input type='text' id='tags' placeholder='React,creative,AI,videography'onChange={(e)=>handleTag(e)} value={tag} className='border-2 border-black'/>
+            <div className='flex flex-row gap-2 w-full overflow-auto'>
+
+            {tags.map((t)=><Tags  key={t} >{t} <span className='ml-2 text-[14px] font-bold 'onClick={()=>removeTag(t)} >x</span></Tags>)}
+            </div>
+            <button onClick={handleTagAdd}>add</button>
+            <label for='video'>Upload video (optional)</label>
+            <input type='file' id='video' className='border-2 border-black'/>
+            <button type='submit' className='bg-blue-500 text-white p-2 rounded'>Create Project</button>
         </form>
       
     </div>
@@ -26,6 +58,7 @@ if(isOpen){
 )
 }
 }
+
 
 
 export default Modal
---
+++ Mytube_frontend/src/components/Tags.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Tags({children}) {
+
+  
+
+  return (
+   
+   <div className='  bg-amber-50 '>#{children}</div>
+   
+  )
+}
+
+export default Tags
